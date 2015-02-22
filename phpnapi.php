0cc13cf56d9e57890d4466fd955150d1079a843b
Jakub Pas <jakub.pas@eniro.com>
2015-02-22 19:32:52 +0100

    fix files extensions

--- phpnapi.php
+++ phpnapi.php
@@ -21,6 +21,9 @@ foreach ($argv as $arg) {
             new RecursiveDirectoryIterator($arg)
         );
         foreach ($fileInfos as $pathname => $fileInfo) {
+            /**
+             * @var SplFileInfo $fileInfo
+             */
             if (!in_array($fileInfo->getExtension(), ['avi', 'mp4', 'rmvb', 'mpeg4', 'mov']) || !$fileInfo->isFile()) {
                 continue;
             }
@@ -42,8 +45,10 @@ function download($file, $lang)
     $md5 = md5(file_get_contents($file, false, null, 0, 10485760));
     $checksum = checksum($md5);
     $url = "http://napiprojekt.pl/unit_napisy/dl.php?l=$lang&f=$md5&t=$checksum&v=other&kolejka=false&nick=&pass=&napios=posix";
-    $compressedFile = reset(explode(".", $file)) . '.7z';
-    $subtitlesFile = reset(explode(".", $file)) . '.srt';
+    $fileInfo = new SplFileInfo($file);
+    $basename = $fileInfo->getPathInfo()->getRealPath().'/'.$fileInfo->getBasename($fileInfo->getExtension());
+    $compressedFile = $basename . '7z';
+    $subtitlesFile = $basename . 'srt';
     $subs = file_get_contents($url);
     if ($subs == 'NPc0') {
         return false;
